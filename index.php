598823496c10859de832af4607151ee9c4dd518c
DinhNguyen <daodinhnguyen1905@gmail.com>
2025-11-18 21:25:20 +0700

    Chinh sua trang chu

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
-sadada
+<?php
+  session_start();
+  header('Content-Type: text/html; charset=utf-8');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="description" content="Khách sạn Quy Nhơn">
     <title>Khách Sạn Quy Nhơn</title>
+    <link rel="stylesheet" href="WebKhachSan/Nhom3/TrangChu/css/thietketrangchu.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
     <header>
@@ -21,12 +29,25 @@ sadada
           <?= htmlspecialchars($_SESSION['fullname']) ?></span> 
        <a href="logout.php" class="dangxuat">Đăng xuất</a>
       <?php else: ?>
-        <a href="DangKy.php" class="dangky"> <i class="fa-solid fa-user" style="color: white;"></i> Đăng ký</a>
+        <a href="WebKhachSan/Nhom3/TrangChu/dangky.php" class="dangky"> <i class="fa-solid fa-user" style="color: white;"></i> Đăng ký</a>
         <a href="DangNhap.php" class="dangnhap"><i class="fa-solid fa-user" style="color: black;"></i>Đăng nhập</a>
       <?php endif; ?>
     </div>
     </header>
+  
+    <div class="container">
+      <div class="content">
+      <h1>Chào mừng đến với trang khách sạn Quy Nhơn năm 2025-2026</h1>
+        <div style="text-align: center;">
+          <img class="anhdautien" src="https://cdn.xanhsm.com/2024/12/fc11ee11-quy-nhon-ve-dem-thumbnail.jpg" alt="Quy Nhơn biển đẹp">
+        </div>
+      </div>
+    </div>
 
-    <div></div>
+<footer id="lienhe">
+  <p>Liên hệ: Nhom3@gmail.com | SĐT: 0812301905</p>
+  <p>&copy; 2025 Khách Sạn Quy Nhơn. Thiết kế bởi Nhóm 3.</p>
+</footer>
+
 </body>
 </html>
